437ad9c9724288f89515c211c860ff149af86dd4
Karima-Aktari <karimaaktari911@gmail.com>
2025-09-16 08:46:20 +0600

    product-details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import AppLayout from "./components/AppLayout";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<AppLayout />}>
             <Route index={true} path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
           </Route>
         </Routes>
       </BrowserRouter>
