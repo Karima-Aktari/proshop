4edd218e62d4c1d418c878f26a90c5ed5bde68d6
Karima-Aktari <karimaaktari911@gmail.com>
2025-09-21 09:04:05 +0600

    product-Controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,39 @@
 import express from "express";
 const router = express.Router();
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
+
 // import products from "../data/products.js";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+// import asyncHandler from "../middleware/asyncHandler.js";
+// import Product from "../models/productModel.js";
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // throw new Error("Some Error");
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductById);
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    // const product = products.find((p) => p._id === req.params.id);
+// router.get(
+//   "/",
+//   asyncHandler(async (req, res) => {
+//     const products = await Product.find({});
+//     // throw new Error("Some Error");
+//     res.json(products);
+//   })
+// );
 
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Resource not found");
-    }
-  })
-);
+// router.get(
+//   "/:id",
+//   asyncHandler(async (req, res) => {
+//     const product = await Product.findById(req.params.id);
+//     // const product = products.find((p) => p._id === req.params.id);
+
+//     if (product) {
+//       return res.json(product);
+//     } else {
+//       res.status(404);
+//       throw new Error("Resource not found");
+//     }
+//   })
+// );
 
 export default router;
