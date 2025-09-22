d63bcdeefd0a6cd1f053141db211216e9d4fc0d2
Karima-Aktari <karimaaktari911@gmail.com>
2025-09-22 09:27:20 +0600

    redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
+import { Provider } from "react-redux";
+import store from "../store.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
