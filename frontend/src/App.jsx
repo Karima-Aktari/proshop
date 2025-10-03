8942aca99a4c4e3bfcce59cfc80d15f2daf380b8
Karima-Aktari <karimaaktari911@gmail.com>
2025-10-03 13:07:12 +0600

    payment-method

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import PrivateRoute from "./components/PrivateRoute";
+import PaymentScreen from "./screens/PaymentScreen";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
 
             <Route path="" element={<PrivateRoute />}>
               <Route path="/shipping" element={<ShippingScreen />} />
+              <Route path="/payment" element={<PaymentScreen />} />
             </Route>
           </Route>
         </Routes>
