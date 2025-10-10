6aeee6ca803afdd15dcd375fb9fc20262334276d
Karima-Aktari <karimaaktari911@gmail.com>
2025-10-10 20:05:03 +0600

    admin-route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,8 @@ import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import AdminRoute from "./components/AdminRoute";
+import OrderListScreen from "./screens/OrderListScreen";
 
 function App() {
   return (
@@ -30,6 +32,10 @@ function App() {
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/order/:id" element={<OrderScreen />} />
               <Route path="/profile" element={<ProfileScreen />} />
+            </Route>
+
+            <Route path="" element={<AdminRoute />}>
+              <Route path="/admin/orderlist" element={<OrderListScreen />} />
             </Route>
           </Route>
         </Routes>
