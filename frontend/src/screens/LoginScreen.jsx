15cc59b0eea0b2d3813e15634c530a7fa4bd6d96
Karima-Aktari <karimaaktari911@gmail.com>
2025-10-13 22:04:48 +0600

    multer-image-upload-endpoint-backend

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -78,7 +78,7 @@ function LoginScreen() {
 
       <Row className="py-3">
         <Col>
-          New Customer ?{" "}
+          New Customer ?
           <Link to={redirect ? `/register?redirect=${redirect}` : "/register"}>
             Register
           </Link>
