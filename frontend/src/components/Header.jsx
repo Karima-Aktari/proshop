97926ab79beafd2536252309d97626a8927ea03c
Karima-Aktari <karimaaktari911@gmail.com>
2025-10-24 20:04:03 +0600

    logo-path-err

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Badge, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import logo from "../assets/logo.png";
+import logo from "/src/assets/logo.png";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import SearchBox from "./SearchBox";
