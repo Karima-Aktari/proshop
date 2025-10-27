cdcfa78bf896222e9495ecd5e681f0b63d4162e3
Karima-Aktari <karimaaktari911@gmail.com>
2025-10-27 22:00:43 +0600

    image-upload-live

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,5 @@
+/** @format */
+
 export const BASE_URL = process.env.NODE_ENV === "development" ? "" : "";
 // export const BASE_URL =
 //   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
@@ -8,4 +10,5 @@ export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
 export const PAYPAL_URL = "/api/config/paypal";
-export const UPLOAD_URL = "/api/upload";
+export const UPLOAD_URL = "/api/gcs-upload";
+// export const UPLOAD_URL = "/api/upload";
